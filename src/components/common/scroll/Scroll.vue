d0a761e3cbd61e9266623b008618914107736f8b
zeus1024 <2982908793@qq.com>
2021-08-27 23:10:16 +0800

    better-scroll

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import BScroll from '@better-scroll/core'
+import Pullup from '@better-scroll/pull-up'
+
+
+ BScroll.use(Pullup);
+
+export default {
+  name:'Scroll',
+  props: {
+    pullUpLoad:{
+      type: Boolean,
+      default: false,
+    },
+    probeType:{
+      type: Number,
+      default:0,
+    },
+  },
+  data(){
+    return {
+      scroll:null,
+    }
+  },
+  mounted(){
+    // 1.创建BScroll对象
+    this.scroll = new BScroll(this.$refs.wrapper,{
+       click:true,
+       pullUpLoad: this.pullUpLoad,
+       probeType: this.probeType,
+       disableTouch:false,
+       disableMouse:false,
+    })
+
+    // 2、监听滚动的位置
+    if(this.probeType === 2 || this.probeType === 3) {
+      this.scroll.on('scroll',(position) => {
+        this.$emit('scrollPosition', position);
+        // console.log(position);
+      })
+
+      // 3、上拉加载更多
+      if(this.pullUpLoad){
+        this.scroll.on('pullingUp',() => {
+          this.$emit('pullingUp');
+        })
+      }
+     
+    }
+  },
+
+  methods: {
+    scrollTo(x,y,time = 500) {
+      this.scroll.scrollTo(x,y,time);
+    },
+    finishPullUp(){
+      this.scroll.finishPullUp();
+    }, 
+    refresh(){
+      this.scroll.refresh();
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,16 +1,28 @@
 <template>
-  <div id="home">
+  <div id="home" class="wrapper" >
     <nav-bar id="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+     
+    <scroll class="content"
+            ref="scroll"
+            :probe-type="3"
+            :pull-up-load="true"
+            @pullingUp="loadMore"
+            @scrollPosition="scrollPosition"
+    >
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend-view :recommends="recommends"></home-recommend-view>
     <feature-view/>
-    <tab-control 
+   <tab-control 
     :titles="['流行', '新款', '精选']"
     @tabClick="tabClick"
+    class="tab-control"
     ></tab-control>
+   
     <goods-list :goods="showGoods"></goods-list>
+    </scroll>
+    <back-top @click.native="backTopClick" v-show="isShowBackTop"></back-top>
   </div>
 </template>
 
@@ -22,16 +34,20 @@ import FeatureView from './childCpn/FeatureView.vue'
 
 
 import NavBar from 'components/common/navbar/NavBar.vue'
+import Scroll from 'components/common/scroll/Scroll.vue'
+
 import TabControl from 'components/content/tabcontrol/TabControl.vue'
 import GoodsList from 'components/content/goodslist/GoodsList.vue'
+import BackTop from 'components/content/backTop/BackTop.vue'
 
 import {getHomeMultidata,getHomeGoods} from 'network/home'
 
 
 
+
 export default {
   name: "home",
-  components:{HomeSwiper,HomeRecommendView,FeatureView,NavBar,TabControl,GoodsList, GoodsList},
+  components:{HomeSwiper,HomeRecommendView,FeatureView,NavBar,Scroll,TabControl,GoodsList, Scroll,BackTop, BackTop},
   data(){
     return {
       banners:[],
@@ -42,10 +58,11 @@ export default {
         'sell':{page:0,list:[]},
       },
       currentType:'pop',
+      isShowBackTop:false,
     }
   },
   created(){
-    this.getHomeMultidata()
+    this.getHomeMultidata();
 
     this.getHomeGoods('pop');
     this.getHomeGoods('new');
@@ -73,6 +90,7 @@ export default {
       }
     },
 
+    
     // 网络请求相关的函数
     getHomeMultidata(){
       getHomeMultidata()
@@ -86,8 +104,27 @@ export default {
       getHomeGoods(type,page)
       .then(res => {
          this.goods[type].list.push(...res.data.list);
-         this.goods[type].page += 1
+        //  处理重复数据
+         let newArr = new Set(this.goods[type].list);
+         this.goods[type].list = Array.from(newArr);	
+        //  
+         this.goods[type].page += 1;
+         this.$refs.scroll.refresh();
+         this.$refs.scroll.finishPullUp();
       })
+    },
+    // 滚动相关方法
+    loadMore(){
+      this.getHomeGoods(this.currentType);
+    },
+
+    // 返回顶部
+    backTopClick(){
+      this.$refs.scroll.scrollTo(0,0,500)
+    },
+    // 获取位置
+    scrollPosition(position){
+      this.isShowBackTop = (-position.y) > 1000;
     }
   }
 }
@@ -96,20 +133,29 @@ export default {
 <style scoped>
 
 #home {
-  padding-top: 44px;
+   height: 100vh;
+   position: relative;
 }
 #home-nav{
   background-color: var(--color-tint);
   color: #fff;
 
   /* 在使用浏览器原生滚动时, 为了让导航不跟随一起滚动*/ 
-    position: fixed;
+    /* position: fixed;
     left: 0;
     right: 0;
     top: 0;
-    z-index: 9;
+    z-index: 9; */
 }
+.content {
+    overflow: hidden;
 
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+  }
 .tab-control {
     position: relative;
     z-index:9;
